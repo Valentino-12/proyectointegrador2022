9e64767fb85b56a92519499c128c1c73864f4511
Mroccia <mroccia@udesa.edu.ar>
2022-06-09 22:49:09 -0300

    validacion del buscador terminado

---
+++ js/search-results.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function(){
+    document.querySelector('#gif-loading').style.display = 'none';
+
+
+
+
+
+
+
+
+
+                       //aca adentro va todo
+
+
+
+
+
+
+
+
+
+
+})
