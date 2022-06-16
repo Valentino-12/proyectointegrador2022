ba5767885f9aa18c29f32b5fd82e502adb54a4ba
Mroccia <mroccia@udesa.edu.ar>
2022-06-16 18:05:57 -0300

    genres terminado

--- js/genres.js
+++ js/genres.js
@@ -1,17 +1,17 @@
 window.addEventListener("load", function() {
 
-    let gener = document.querySelector('.genero');
+    let gener = document.querySelector('.all-gender');
 
     fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/genre')
-    .then(function(datos){
-        return datos.json();
+    .then(function(response) {
+        return response.json();
     })
+    
     .then(function(info){
         console.log(info.data);
-        console.log(info.data[0].id);
 
-        for(let i = 0; i <= info.data.id ; i++){
-            gener.innerHTML = `<a href="./detail-geners.html"><img src="${info.data[i].picture_big}"><b>?${info.data[i].name}</b></a>`
+        for(let i = 0; i <= info.data.length ; i++){
+            gener.innerHTML += `<article class="genero"><a href="./detail-geners.html"><img src="${info.data[i].picture}"><br><b>${info.data[i].name}</b></a></article>`
         }
     })
     .catch(function(error){
