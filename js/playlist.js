e9278f81c52873eb34fc5ca463ae12c297f62a82
Mroccia <mroccia@udesa.edu.ar>
2022-06-20 04:35:49 -0300

    avance favorito

--- js/playlist.js
+++ js/playlist.js
@@ -1,40 +1,33 @@
 window.addEventListener("load", function() {
 
     if(sessionStorage.getItem("pref") != null) {
-        let playli = document.querySelector('.no-hay');
+
+        let playList = document.querySelector(".playlist");
         let añadir = sessionStorage.getItem("pref").split(",")
 
         for (let i = 0; i < añadir.length; i++) {
 
             fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/track/' + añadir[i])
-            .then(
-                function(response) {
-                    return response.json();            
-                }
-            )   
-            .then(
-                function(resultado) {
-                    console.log(resultado)
-                    
-                    playli.innerHTML += `<article class="song">
-                    <p class="nomer"> - </p>
-                    <a href="detail-track.html"> <img class="pos" src= "${info.album.cover_medium}>
-                    <p class="nameplay">${info.title}</p></a>
-                    </article>`   
-
+            .then(function(response) {
+                return response.json();
+            })   
+            .then(function(result) {
+                console.log(result)
+                playList.innerHTML += `<article class="song">
+                <a href="detail-track.html?id=${result.id}"> <img class="pos" src="${result.album.cover}">
+                <p class="nameplay">${info.title}</p></a>
+                </article>`
                 document.querySelector(".remove").addEventListener("click", function(){
                     let remove = sessionStorage.removeItem("pref")
                     console.log(remove)
-
                 })
-                }
-            )
+            })
         }
 
     } else {    
-        playli.innerHTML = `<section class="songen"><p class="nums"> - 
-        </p><img class="imgnosignal" src="../img/no signal.jpg">
+        let nohay = `<section class="songen"><img class="imgnosignal" src="../img/no signal.jpg">
         <p class="nameplay">Parece que no hay canciones agregadas a tu playlist por el momento</p></a></section>`
+        playList.innerHTML = nohay
     }
 })
 
